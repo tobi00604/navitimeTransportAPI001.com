011c5b2cdb1053e78f4f8809ccfa2efcedee628f
tobi00604 <50732577+tobi00604@users.noreply.github.com>
2025-06-02 16:18:30 +0900

    Update fetch-data.js

--- netlify/functions/fetch-data.js
+++ netlify/functions/fetch-data.js
@@ -1,32 +1,23 @@
 // netlify/functions/fetch-data.js
 
 export async function handler(event, context) {
-  const API_KEY = process.env.RAPIDAPI_KEY;   // Netlify側に設定
-  const API_HOST = process.env.RAPIDAPI_HOST; // Netlify側に設定
-
-  const endpoint = event.queryStringParameters.endpoint;
-  if (!endpoint) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({ error: 'Missing "endpoint" parameter' }),
-    };
-  }
 
   let url;
 
   // エンドポイントに応じてURLを分岐
+  const endpoint = event.queryStringParameters.endpoint;
   switch (endpoint) {
     case 'first':
       const word = event.queryStringParameters.word;
-      url = `https://navitime-transport.p.rapidapi.com/transport_node?coord_unit=degree&word=` + word + `&offset=0&datum=wgs84&limit=10`;
+      url = `https://navitime-transport.p.rapidapi.com/transport_node?coord_unit=degree&word=${word}&offset=0&datum=wgs84&limit=10`;
       break;
     case 'second':
       const stationId = event.queryStringParameters.stationId;
-      url = `https://navitime-transport.p.rapidapi.com/transport_node/id?id=` + stationId + `&datum=wgs84&coord_unit=degree&options=detail`;
+      url = `https://navitime-transport.p.rapidapi.com/transport_node/id?id=${stationId}&datum=wgs84&coord_unit=degree&options=detail`;
       break;
     case 'third':
       const routeId = event.queryStringParameters.routeId;
-      url = `https://navitime-transport.p.rapidapi.com/transport_link/id?options=node&id=` + routeId + `&coord_unit=degree&datum=wgs84`;
+      url = `https://navitime-transport.p.rapidapi.com/transport_link/id?options=node&id=${routeId}&coord_unit=degree&datum=wgs84`;
       break;
     default:
       return {
@@ -39,8 +30,8 @@ export async function handler(event, context) {
     const response = await fetch(url, {
       method: 'GET',
       headers: {
-        'x-rapidapi-key': API_KEY,
-        'x-rapidapi-host': API_HOST,
+        'x-rapidapi-key': process.env.RAPIDAPI_KEY,
+        'x-rapidapi-host': process.env.RAPIDAPI_HOST,
       }
     });
 
